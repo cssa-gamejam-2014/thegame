d23426e8d27b80948cd431f9c94a82d43ab0b440
Jarrah <themrb4@gmail.com>
2014-04-05 15:51:51 +1100

    men being drawn

--- game.js
+++ game.js
@@ -1,47 +1,57 @@
-// Draw a person
-function drawPerson(outfit, position){
-    var c=document.getElementById("examples");
-    var ctx=c.getContext("2d");
-    var body=document.getElementById("body");
-    var hat=document.getElementById("hat");
-    ctx.drawImage(body,10,10);
-    ctx.drawImage(hat,10,10);
-}
 
-function getRandom(range){
-    return Math.floor((Math.random()*range));
-}
-
-colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-
-level1 = {
-    generator : function(){
-        person = []
+$(document).ready(function(){
+    // Draw a person
+    function drawPerson(outfit, position){
+        var c=document.getElementById("myCanvas");
+        console.log(c);
+        var ctx=c.getContext("2d");
+        var body=document.getElementById("body");
+        var hat=document.getElementById("hat");
         
-        chanceOfWear = {hat: 100, shirt: 100, pants: 100, jacket: 100, shoes: 100}
-        var clothes = ['hat', 'pants', 'jacket', 'socks', 'shoes'];
-        clothes.forEach(function(entry){
-            if (getRandom(100) < chanceOfWear[entry]){
-                person[entry] =  colours[getRandom(6)];
+        posOffset = (position-1)*200+12
+        ctx.drawImage(body,posOffset,10);
+        ctx.drawImage(hat,posOffset,10);
+    }
+
+    function getRandom(range){
+        return Math.floor((Math.random()*range));
+    }
+
+    colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+    level1 = {
+        generator : function(){
+            person = []
+            
+            chanceOfWear = {hat: 100, shirt: 100, pants: 100, jacket: 100, shoes: 100}
+            var clothes = ['hat', 'pants', 'jacket', 'socks', 'shoes'];
+            clothes.forEach(function(entry){
+                if (getRandom(100) < chanceOfWear[entry]){
+                    person[entry] =  colours[getRandom(6)];
+                }
+            });
+            
+            if (getRandom(2) == 1){
+                person['shirt'] = 'green';
+            } else {
+                person['shirt'] = 'blue';
             }
-        });
-        
-        if (getRandom(2) == 1){
-            person['shirt'] = 'green';
-        } else {
-            person['shirt'] = 'blue';
-        }
-        
-        
-        return person;
-    },
-    validator : function(clothing){
-        if (clothing.shirt == 'blue' || clothing.shirt == 'green'){
-            return true;
-        } else {
-            return false;
+            
+            
+            return person;
+        },
+        validator : function(clothing){
+            if (clothing.shirt == 'blue' || clothing.shirt == 'green'){
+                return true;
+            } else {
+                return false;
+            }
         }
     }
-}
 
-console.log(level1.generator());
+    console.log(document.getElementById("myCanvas"));
+    drawPerson(level1.generator(), 1);
+    drawPerson(level1.generator(), 2);
+    drawPerson(level1.generator(), 3);
+
+});
